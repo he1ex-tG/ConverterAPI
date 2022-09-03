af721f1f38db6a560d441e2f9fd9a3b331b74c03
he1ex-tG <gosu.he1ex@gmail.com>
2022-09-04 01:05:03 +0500

    Initial commit

---
+++ src/main/kotlin/com/he1extg/ConverterAPI/ConverterApiApplication.kt
@@ -0,0 +1,11 @@
+package com.he1extg.ConverterAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConverterApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConverterApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/he1extg/ConverterAPI/ConverterApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.he1extg.ConverterAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConverterApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
