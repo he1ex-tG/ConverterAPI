6a2d7c3a46523a708d79844c717df715638f5269
he1ex-tG <gosu.he1ex@gmail.com>
2022-10-09 15:58:44 +0500

    Changed return type of file conversion

--- src/main/kotlin/com/he1extg/converterapi/controller/ConverterController.kt
+++ src/main/kotlin/com/he1extg/converterapi/controller/ConverterController.kt
@@ -2,9 +2,6 @@ package com.he1extg.converterapi.controller
 
 import com.he1extg.converterapi.converter.Converter
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.ByteArrayResource
-import org.springframework.core.io.Resource
-import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
